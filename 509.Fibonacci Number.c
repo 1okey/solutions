89a16afd83950f34052fac7d17dbec65e39ac66d
AntonSkochko <antonskochko@gmail.com>
2019-03-12 16:23:20 +0200

    added memoization for fibonacci problem

--- 509.Fibonacci Number.c
+++ 509.Fibonacci Number.c
@@ -1,18 +1,10 @@
-int fib(int N) {
-    return N < 2 ? N : fib(N - 1) + fib(N - 2);
-}
+static int memo[31];
 
-int fib(int N)
-{
-	if(N<2) return N;
-	
-	int prev = 0, curr = 1, next;
-	for(int i = 2;i <= N;i++)
-	{
-		next = prev + curr;
-		prev = curr;
-		curr = next;
-	}
-	
-	return next;
+int fib(int N) {
+    if(N < 2)    
+        return N;
+    if(!memo[N]) 
+        return (memo[N] = fib(N - 1) + fib(N - 2));
+
+    return memo[N];
 }
