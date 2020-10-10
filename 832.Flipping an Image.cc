3339c27fd36677f00ab8a5191a35454e3e2a75da
anton-skochko <anton.skochko@vimeo.com>
2020-10-10 21:23:19 +0300

    some solutions

--- 832.Flipping an Image.cc
+++ 832.Flipping an Image.cc
@@ -1,25 +1,19 @@
-static int asyncx = [](){
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    return 0;
-}();
+// https://leetcode.com/problems/flipping-an-image/submissions/
 
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
-        for(auto& row: A){
-            for( size_t i = 0, j = row.size() - 1;
-                i <= row.size() / 2  && i <= j
-                ;++i, --j ){
-                int& a = row[i];
-                int& b = row[j];
-                swap(a,b);
-                a = !a;
-                if(i != j)
-                    b = !b;
+        vector<vector<int>> B;
+        for(vector<int>& row : A)
+        {
+            vector<int> reversed(row.size());
+            for(size_t i = 0; i < row.size(); ++i)
+            {
+                int bit = row[row.size() - i - 1];
+                reversed[i] = !bit;
             }
+            B.push_back(reversed);
         }
-        return A;
+        return B;
     }
 };
-
